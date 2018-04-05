c3adc44caa7910d1187d7c005a2cf36a46d5440f
jrm51c <jmccarthyn2n@yahoo.com>
2018-04-05 12:54:02 -0500

    jQuery issue with external javascript for Dexter to review

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+
+
+   // Here we created an on-click event that responds to button clicks of the crystal image.
+   $("#crystals").on("click", ".crystal-image", function() {
+
+    // Clicking the button triggers an alert message.
+    alert("You clicked a crystal!");
+
+  });
+
+});
